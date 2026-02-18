56f928399ee79b8703772257415d9a94746018ef
josevictorbdbr <josevictorbdbr@gmail.com>
2026-02-18 12:00:35 -0300

    Added basic files

---
+++ src/test/java/com/josevictor/productmanager/ProductmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.josevictor.productmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductmanagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
